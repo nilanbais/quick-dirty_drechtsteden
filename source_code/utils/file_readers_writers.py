a857dd744cd49441b942d64f68051d4d56e5b289
Bais <n.bais@drechtsteden.nl>
2025-07-24 15:43:17 +0200

    add file readers + writers

---
+++ source_code/utils/file_readers_writers.py
@@ -0,0 +1,20 @@
+"""
+File met objecten voor het lezen van bestanden.
+"""
+import json
+
+
+def read_json(file_path: str) -> dict:
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            json_data = json.load(f) 
+        return json_data
+    except FileNotFoundError:
+        print("Bestand niet gevonden.")
+    except json.JSONDecodeError as e:
+        print("JSON is ongeldig:", e)
+
+
+def write_to_json(json_data: dict, file_path: str) -> None:
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(json_data, f, indent=4)
