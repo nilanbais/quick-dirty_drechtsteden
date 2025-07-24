346712c7f979f7481e730f29317c509431206b83
Bais <n.bais@drechtsteden.nl>
2025-07-24 19:28:26 +0200

    add input manager test

---
+++ tests/test_input_manager.py
@@ -0,0 +1,13 @@
+"""
+File met tests voor de input manager.
+"""
+import os
+
+from source_code.services.input_manager import InputManager
+
+from dotenv import load_dotenv
+load_dotenv()
+
+def test_previous_handles_sets():
+    manager_object = InputManager()
+    assert manager_object.prev_handled_files != None
