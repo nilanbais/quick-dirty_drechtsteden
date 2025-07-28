1ce8eb1edd08c99c4db707651d519c54ac59fba8
Bais <n.bais@drechtsteden.nl>
2025-07-28 18:56:19 +0200

    add utils test

---
+++ tests/test_utils.py
@@ -0,0 +1,12 @@
+"""
+File met tests voor de utility functies.
+"""
+import os
+
+from source_code.utils.aggregation_utils import file_extention_from_path
+
+
+def test_file_extention_from_path():
+    path: str = "tests\test_data_manager.py"
+    file_extention: str = file_extention_from_path(path)
+    assert file_extention == 'py'
