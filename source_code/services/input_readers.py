be12a0d920f1cdb5a0ea4dc0b14d1d49a85c12ad
Bais <n.bais@drechtsteden.nl>
2025-07-28 16:07:37 +0200

    reneme output variable

--- source_code/services/input_readers.py
+++ source_code/services/input_readers.py
@@ -8,9 +8,9 @@ from pandas import DataFrame
 def dataset_reader(path: str, file_extention: str) -> DataFrame:
     match file_extention:
         case 'xlsx':
-            input_df: DataFrame = pd.read_excel(path, header=1)
-        case 'csv':
-            input_df: DataFrame = pd.read_csv(path, header=1)
+            dataset: DataFrame = pd.read_excel(path, header=1)
+        case 'csv' | '.csv':
+            dataset: DataFrame = pd.read_csv(path, header=1)
         case _:
             raise ValueError("No suitable file_extention given. Choose 'csv', 'xlsx'.")
-    return input_df
+    return dataset
