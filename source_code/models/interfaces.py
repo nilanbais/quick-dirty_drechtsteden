3c17c98ca1e59aaac5a415d890c21777f44f16f8
Bais <n.bais@drechtsteden.nl>
2025-07-28 19:40:05 +0200

    feat: add empty dataset when none stored

--- source_code/models/interfaces.py
+++ source_code/models/interfaces.py
@@ -1,8 +1,67 @@
 """
 File met objecten die fungeren als interface tussen de verschillende lagen van de automatisering.
 """
-from dataclasses import dataclass
+from typing import List
 
-@dataclass
+
 class TransformationInputInterface:
-    pass
+    columns: List[str] = [
+        'CallId',
+        'Segment',  
+        'Start',
+        'Stop',
+        'Calling Party',
+        'Trunk Group',
+        'Dialed Number',   
+        'Answered Agent',  
+        'Original Agent',
+        'Call Disposition', 
+        'Vector',        
+        'Priority',
+        'Split',  
+        'Level', 
+        'System',
+        'Queue',   
+        'Ring',   
+        'Talk',
+        'Hold',    
+        'ACW',
+        'Consult',
+        'Disposition',        
+        'Duration',                  
+        1,
+        2,                  
+        3,                  
+        4,
+        5,                  
+        6,                  
+        7,
+        8,                  
+        9,    
+        '1.1',
+        '2.1',    
+        '3.1',    
+        '4.1',
+        '5.1'
+    ]
+
+
+class ReportDatasetInterface:
+    columns: List[str] = [
+        "Datum",        	
+        "Gemiddelde Antwoord Snelheid,"
+        "Gemiddelde Annuleer-tijd",
+        "Aangeboden",
+        "ACD-Oproepen",
+        "Gemiddelde ACD-tijd",
+        "Gemiddelde ACW-tijd",
+        "Geannuleerde Oproepen",
+        "Maximale Vertraging",
+        "Maximale In-wachtrij",
+        "Extensie Uit-gesprek",
+        "Gemiddelde Extensie Uit-gesprek",
+        "ACD-Tijd (%)",
+        "Beantwoorde Oproepen (%)",
+        "Binnen Service-Level (%)",
+        "Omgeleid Geen Antwoord"
+    ]
--- source_code/services/output_manager.py
+++ source_code/services/output_manager.py
@@ -10,6 +10,7 @@ from pandas import DataFrame
 
 from source_code.utils.file_readers_writers import dataset_reader
 from source_code.utils.string_utils import file_extention_from_path
+from source_code.models.interfaces import ReportDatasetInterface
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,10 +22,10 @@ OUTPUT_FOLDER_PATH: str = os.getenv("OUTPUT_FOLDER_PATH")
 
 class OutputManager:
 
-    output_folder: str = os.path.join(OUTPUT_FOLDER_PATH, 'rapportage_dataset.csv')
+    stored_dataset_path: str = os.path.join(OUTPUT_FOLDER_PATH, 'rapportage_dataset.csv')
 
     def __init__(self) -> None:
-        self.dataset: DataFrame = dataset_reader(path=self.output_folder, file_extention=file_extention_from_path(self.output_folder))
+        self.dataset: DataFrame = self.load_stored_dataset()
 
     def add(self, new_data: DataFrame) -> None:
         self.dataset = pd.concat([self.dataset, new_data], ignore_index=True)
@@ -32,7 +33,11 @@ class OutputManager:
     def store_dataset(self, mode: str = 'overwrite') -> None:
         match mode:
             case 'overwrite':
-                self.dataset.to_csv(self.output_folder, index=False, encoding='utf-8')
+                self.dataset.to_csv(self.stored_dataset_path, index=False, encoding='utf-8')
 
-    def load_stored_dataset(path: str) -> DataFrame:
-        pass
+    def load_stored_dataset(self) -> DataFrame:
+        if os.path.exists(self.stored_dataset_path):
+            stored_dataset: DataFrame = dataset_reader(path=self.stored_dataset_path, file_extention=file_extention_from_path(self.stored_dataset_path))
+        else:
+            stored_dataset = DataFrame(columns=ReportDatasetInterface.columns)
+        return stored_dataset
